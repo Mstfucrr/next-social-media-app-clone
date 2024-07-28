bd25c30eca9d5489f029df7a34b883badfaafb32
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2024-07-28 23:13:03 +0300

    Add main view components and layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,14 @@
+import { validateRequest } from '@/views/auth/lib/auth'
+import { redirect } from 'next/navigation'
+
 // src/app/(auth)/layout.tsx
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
+export default async function AuthLayout({ children }: { children: React.ReactNode }) {
+  const { user } = await validateRequest()
+
+  if (user) redirect('/')
+
   return (
-    <main className='flex-center h-screen'>
+    <main className='h-screen flex-center'>
       <div className='flex h-full max-h-[540px] w-full max-w-5xl overflow-hidden rounded-2xl bg-card shadow-2xl'>
         {children}
       </div>
