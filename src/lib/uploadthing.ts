fd824bc0228fa707b0e13edd7e596c6bb945acdc
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2024-08-05 15:23:38 +0300

    Update import path for AppFileRouter in uploadthing.ts

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,4 +1,4 @@
-import type { AppFileRouter } from '@/app/api/users/uploadthing/core'
+import type { AppFileRouter } from '@/app/api/uploadthing/core'
 import { generateReactHelpers } from '@uploadthing/react'
 
 export const { useUploadThing, uploadFiles } = generateReactHelpers<AppFileRouter>()
