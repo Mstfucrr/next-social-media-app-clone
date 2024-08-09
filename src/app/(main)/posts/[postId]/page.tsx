165f927fae2cdb1a4c901f4ae92cda775aacfb91
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2024-08-09 03:12:50 +0300

    Add new post detail page with view and add user info sidebar component

---
+++ src/app/(main)/posts/[postId]/page.tsx
@@ -0,0 +1,45 @@
+import prisma from '@/lib/prisma'
+import { validateRequest } from '@/views/auth/lib/auth'
+import PostDetailView from '@/views/main/posts/post'
+import { getPostDataInclude } from '@/views/main/posts/utils'
+import { notFound } from 'next/navigation'
+import { cache } from 'react'
+
+interface PostPageProps {
+  params: { postId: string }
+}
+
+const getPost = cache(async (postId: string, loggedInUserId: string) => {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: postId
+    },
+    include: getPostDataInclude(loggedInUserId)
+  })
+
+  if (!post) return notFound()
+
+  return post
+})
+
+export async function generateMetadata({ params }: PostPageProps) {
+  const { user } = await validateRequest()
+
+  if (!user) return {}
+
+  const post = await getPost(params.postId, user.id)
+
+  return {
+    title: `${post.user.displayName}: ${post.content.slice(0, 50)}...`
+  }
+}
+
+export default async function PostPage({ params }: PostPageProps) {
+  const { user } = await validateRequest()
+
+  if (!user) return <p className='text-destructive'>You need to be logged in to view this page</p>
+
+  const post = await getPost(params.postId, user.id)
+
+  return <PostDetailView post={post} />
+}
---
+++ src/views/main/posts/components/UserInfoSidebar.tsx
@@ -0,0 +1,50 @@
+import { UserData } from '@/types'
+import { validateRequest } from '@/views/auth/lib/auth'
+import Link from 'next/link'
+import FollowButton from '../../components/FollowButton'
+import Linkify from '../../components/Linkify'
+import UserAvatar from '../../components/UserAvatar'
+import UserTooltip from '../../components/UserTooltip'
+
+interface UserInfoSidebarProps {
+  user: UserData
+}
+
+export default async function UserInfoSidebar({ user }: UserInfoSidebarProps) {
+  const { user: loggedInUser } = await validateRequest()
+
+  if (!loggedInUser) return null
+
+  return (
+    <div className='space-y-5 rounded-2xl bg-card p-5 shadow-sm'>
+      <h2 className='text-xl font-bold'>About this user</h2>
+      <UserTooltip user={user}>
+        <Link href={`/users/${user.username}`} className='flex items-center gap-3'>
+          <UserAvatar avatarUrl={user.avatarUrl} className='flex-none' />
+          <div>
+            <span className='line-clamp-1 break-all font-semibold hover:underline'>
+              {user.displayName}
+            </span>
+            <span className='line-clamp-1 break-all text-muted-foreground'>@{user.username}</span>
+          </div>
+        </Link>
+      </UserTooltip>
+
+      <Linkify>
+        <p className='line-clamp-6 whitespace-pre-line break-words text-muted-foreground'>
+          {user.bio}
+        </p>
+      </Linkify>
+
+      {user.id !== loggedInUser.id && (
+        <FollowButton
+          initialState={{
+            followers: user._count.followers,
+            isFollowdUser: user.followers.some(follower => follower.followerId === loggedInUser.id)
+          }}
+          userId={user.id}
+        />
+      )}
+    </div>
+  )
+}
